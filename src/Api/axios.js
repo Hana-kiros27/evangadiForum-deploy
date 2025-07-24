7a200e5d828b8feda1a80ef16f62159c3bd5357d
Hana <159703054+Hana-kiros27@users.noreply.github.com>
2025-07-24 20:48:32 +0200

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
   // version of localhost
-  baseURL:"https://forum-api-deploy-20.onrender.com/api"
+  baseURL:"https://evangadiforum-be.kelilmohamad.com/api"
   // baseURL: "http://localhost:3000/api",
 
   // deployed version of Evangadi server in Render.com
