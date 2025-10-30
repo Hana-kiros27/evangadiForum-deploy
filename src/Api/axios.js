d344e0c5505e17d6341d84eae28e7e4b93de3d9e
Hana Hailekiros <hanahailekiros27@gmail.com>
2025-10-30 10:46:11 +0200

    111

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,8 +3,8 @@ const instance = axios.create({
   // version of localhost
   // baseURL:"https://forum-api-deploy-20.onrender.com/api"
 
-    // baseURL:"https://evangadiforum-be.kelilmohamad.com/api"
-  baseURL: "http://localhost:3000/api",
+    baseURL:"https://evangadiforum-be.kelilmohamad.com/api"
+  // baseURL: "http://localhost:3000/api",
 
 });
 export default instance;
